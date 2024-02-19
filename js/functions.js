69b9fd6d029f34eee8e3567d6dc0b89ca74dce0b
Alimov Farruh <linsheng1990@gmail.com>
2024-02-19 23:19:21 +0200

    Вызывает функции, так как линтер ругается)

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,18 @@ function checkStringLength (text, number) {
   return text.length <= number;
 }
 
+checkStringLength('проверяемая строка', 20);
+
 function checkPalindrome (text) {
   text = text.replaceAll(' ', '').toLowerCase();
   const reversedText = text.split('').reverse().join('');
   return reversedText === text;
 }
 
+checkPalindrome('Лёша на полке клопа нашёл ');
+
 function getNumbers (text) {
   return parseInt(text.replace(/[^0-9]/g,''), 10);
 }
+
+getNumbers('1 кефир, 0.5 батона');
