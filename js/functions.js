4e69a10423215728d80c95e84c086823605a1f51
Alimov Farruh <linsheng1990@gmail.com>
2024-02-19 23:02:35 +0200

    Вносит правки и добавляет 3ую функцию

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,13 @@
-function getStringLengrh (string, number) {
-  if (string.length <= number) {
-    return true;
-  }
-
-  return false;
+function checkStringLength (text, number) {
+  return text.length <= number;
 }
 
-// Строка короче 20 символов
-console.log(getStringLengrh('проверяемая строка', 20)); // true
-// Длина строки ровно 18 символов
-console.log(getStringLengrh('проверяемая строка', 18)); // true
-// Строка длиннее 10 символов
-console.log(getStringLengrh('проверяемая строка', 10)); // false
-
-function checkPalindrome (string) {
-  string = string.replaceAll(' ', '');
-  string = string.toLowerCase();
-
-  const standartlWord = string;
-  let invertedWord = [];
-
-  for (let i = standartlWord.length - 1; i !== -1; i--) {
-    invertedWord += standartlWord[i];
-  }
-
-  if (invertedWord === standartlWord) {
-    return true;
-  }
-
-  return false;
+function checkPalindrome (text) {
+  text = text.replaceAll(' ', '').toLowerCase();
+  const reversedText = text.split('').reverse().join('');
+  return reversedText === text;
 }
 
-// Строка является палиндромом
-console.log(checkPalindrome('топот')); // true
-// Несмотря на разный регистр, тоже палиндром
-console.log(checkPalindrome('ДовОд')); // true
-// Это не палиндром
-console.log(checkPalindrome('Кекс')); // false
-// Это палиндром
-console.log(checkPalindrome('Лёша на полке клопа нашёл ')); // true
+function getNumbers (text) {
+  return parseInt(text.replace(/[^0-9]/g,''), 10);
+}
