ba5a59c58b1e780ab64c34646140cf77c7aa91d4
Alimov Farruh <linsheng1990@gmail.com>
2024-03-05 00:45:53 +0200

    Вносит правки

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const DESCRIPTION = [
   'Лесной туман',
   'Играющие дети',
   'Заснеженные горы',
-  'Поле подсолнухов'
+  'Поле подсолнухов',
 ];
 
 const COMMENTS = [
@@ -30,15 +30,31 @@ const COMMENTS = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
+
+const NUMBER_PUBLICATION = 25;
+
+const MIN_RANGE_AVATAR = 1;
+const MAX_RANGE_AVATAR = 6;
+
+const MIN_MESSAGES = 1;
+const MAX_MESSAGES = 2;
+
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
+
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
 
 const getRandomInteger = (min, max) => {
   const randomInteger = min + Math.random() * (max + 1 - min);
   return Math.floor(randomInteger);
 };
 
-const getRandomArrayElements = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const getNumberСopies = (object, min, max) => Array.from({length: getRandomInteger(min, max)}, object);
 
 const getCount = (count) => function () {
   return count++;
@@ -47,16 +63,33 @@ const getCount = (count) => function () {
 const countId = getCount(1);
 const countUrl = getCount(1);
 
-const createComment = () => ({
+const getRandomeMessage = (array) => {
+  let message = getRandomArrayElement(array);
+  const numberMessages = getRandomInteger(MIN_MESSAGES, MAX_MESSAGES);
+
+  if (numberMessages === MAX_MESSAGES) {
+    message = message.concat([getRandomArrayElement(array)]);
+    return message;
+  }
+
+  return message;
+};
+
+const publicationComment = () => ({
   id: countId(),
-  url: `photos/${ countUrl() }.jpg`,
-  description: getRandomArrayElements(DESCRIPTION),
-  likes: getRandomInteger(15, 200),
-  avatar: `img/avatar-${ getRandomInteger(1, 6) }.jpg`,
-  comments: getRandomArrayElements(COMMENTS),
-  userName: getRandomArrayElements(NAMES),
+  avatar: `img/avatar-${ getRandomInteger(MIN_RANGE_AVATAR, MAX_RANGE_AVATAR) }.jpg`,
+  message: getRandomeMessage(COMMENTS),
+  name: getRandomArrayElement(NAMES),
 });
 
-const similarComments = Array.from({length: 25}, createComment);
+const publicationPhoto = () => ({
+  url: `photos/${ countUrl() }.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+  comment: getNumberСopies(publicationComment, MIN_COMMENTS, MAX_COMMENTS),
+});
 
-// console.table(similarComments);
+const simulatorPublicationPhoto = Array.from({length: NUMBER_PUBLICATION}, publicationPhoto);
+
+// console.table(simulatorPublicationPhoto);
+// console.log(getNumberСopies(publicationComment, MIN_COMMENTS, MAX_COMMENTS));
